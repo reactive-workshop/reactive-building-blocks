c3517c3c5fadac4f12cd6708e75b20f8058d26c7
Kumar <aquaraga@gmail.com>
2022-10-26 12:47:10 +0530

    map and flatMap examples

---
+++ src/test/java/com/example/reactivebuildingblocks/basicoperators/FlatMapTests.java
@@ -0,0 +1,58 @@
+package com.example.reactivebuildingblocks.basicoperators;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+public class FlatMapTests {
+
+    @Test
+    public void doubleTheNumbersWithFlatMap() {
+
+        Flux<Integer> numbers = Flux.just(1, 2, 3, 4);
+
+        Flux<Integer> doubledNumbers = numbers.flatMap(x -> doubleTheNumber(x));
+
+        Assertions.assertEquals(List.of(2, 4, 6, 8), doubledNumbers.collectList().block());
+    }
+
+    private Mono<Integer> doubleTheNumber(int number) {
+        return Mono.just(number * 2);
+    }
+
+    @Test
+    public void colorOfFruitsWithFlatMap() {
+
+        Flux<String> fruits = Flux.just("apple", "orange", "grapes", "watermelon");
+
+        //TODO. Use the utility function colorOf
+        Flux<String> fruitColors = fruits;
+
+        Assertions.assertEquals(List.of("red", "orange", "purple", "green"), fruitColors.collectList().block());
+    }
+
+    private Mono<String> colorOf(String fruit) {
+
+        Map<String, String> allFruits = Map.of("apple", "red",
+                "orange", "orange",
+                "grapes", "purple",
+                "watermelon", "green");
+        return Mono.just(allFruits.get(fruit));
+    }
+
+    @Test
+    public void colorOfMixedCasedFruitsWithFlatMap() {
+
+        Flux<String> fruits = Flux.just("aPPle", "Orange", "graPes", "waterMeLon");
+
+        //TODO. Map to lowercase and then apply flatMap over colorOf
+        Flux<String> fruitColors = fruits;
+
+        Assertions.assertEquals(List.of("red", "orange", "purple", "green"), fruitColors.collectList().block());
+    }
+
+}
---
+++ src/test/java/com/example/reactivebuildingblocks/basicoperators/MapTests.java
@@ -0,0 +1,32 @@
+package com.example.reactivebuildingblocks.basicoperators;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public class MapTests {
+
+    @Test
+    public void doubleTheNumbersWithMap() {
+
+        Flux<Integer> numbers = Flux.just(1, 2, 3, 4);
+
+        Flux<Integer> doubledNumbers = numbers.map(x -> x * 2);
+
+        Assertions.assertEquals(List.of(2, 4, 6, 8), doubledNumbers.collectList().block());
+    }
+
+    @Test
+    public void upperCaseStringsWithMap() {
+
+        Flux<String> fruits = Flux.just("aPPle", "Orange", "graPes", "waterMeLon");
+
+        //TODO
+        Flux<String> upperCasedFruits = fruits;
+
+        Assertions.assertEquals(List.of("APPLE", "ORANGE", "GRAPES", "WATERMELON"), upperCasedFruits.collectList().block());
+    }
+
+}
